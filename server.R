6e29b85cb7eec57589e538ca2bab18b470f1aa09
Mike Russell <mike.russell@gov.ab.ca>
2017-07-06 16:36:25 -0600

    Update server.R

--- server.R
+++ server.R
@@ -164,7 +164,7 @@ GIS <- reactive(input$WMU_Shp$datapath[1])
     print(paste("GIS Input=", GISInput)) #input$WMU_Shp$datapath[1])
     # GISInput <- "F:/GIS_Workspace/R_Files/A_359_Boundary_TTM.shp"
 
-    survey.area359.TTM <- readOGR(input$WMU_Shp$datapath[1], substr(basename(GISInput),1,nchar(basename(GISInput))-4))
+    survey.area359.TTM <- readOGR(input$WMU_Shp$datapath[1], input$WMU_Shp$Name)#substr(basename(GISInput),1,nchar(basename(GISInput))-4))
     # survey.areanon355 <- readOGR(dsn=StrataPolyLayerFile, layer=substr(basename(StrataPolyLayerFile),1,nchar(basename(StrataPolyLayerFile))-4))
     # survey.transects359.TTM <- readOGR(dsn=PolyLineTransflown, layer=substr(basename(PolyLineTransflown),1,nchar(basename(PolyLineTransflown))-4))
 
