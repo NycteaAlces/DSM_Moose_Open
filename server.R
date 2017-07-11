296a146b2388b256b6756fdec5226e835a98f73c
Mike Russell <mike.russell@gov.ab.ca>
2017-07-11 11:06:49 -0600

    Update server.R

--- server.R
+++ server.R
@@ -167,7 +167,7 @@ GIS <- reactive(input$WMU_Shp)
       #Handle the file names such that Shiny doesn't get confused with shapefiles
 
 GetShapefile <- function(id, InShapefile, OutShapefile){
-    ns <- NS(OutShapefile)
+   # ns <- NS(OutShapefile)
     if (is.null(InShapefile)) 
         return(NULL)  
     dir<-dirname(InShapefile[1,4])
