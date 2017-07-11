4df6b4e114f293bf09bb78bf0be1a10b1f7a561a
Mike Russell <mike.russell@gov.ab.ca>
2017-07-11 10:06:27 -0600

    Update server.R

--- server.R
+++ server.R
@@ -173,8 +173,10 @@ GIS <- reactive(input$WMU_Shp$datapath)#[6])
       print(paste("Directory name:",dir))
     for ( i in 1:nrow(myshape)) {
     file.rename(myshape[i,4], paste0(dir,"/",myshape[i,1]))}
-    
+      
     getshp <- list.files(dir, pattern="*.shp", full.names=TRUE)
+      
+      print(paste("Shapefile name:",getshp))
       shape<-readShapePoly(getshp)
       
     survey.area359.TTM <- readShapePoly(shape)
