850e5274b5292a903b09305c03ab3a65c975ce48
Mike Russell <mike.russell@gov.ab.ca>
2017-07-11 09:40:29 -0600

    Update server.R

--- server.R
+++ server.R
@@ -27,7 +27,7 @@ shinyServer(function(input, output,session) {
 
 #Prepare the user-input slots -- dynamic/reactive
 DB <- reactive(input$MegaDB$datapath)
-GIS <- reactive(input$WMU_Shp$datapath[1])
+GIS <- reactive(input$WMU_Shp$datapath[6])
 
   output$myplot <- renderPlot({
 
@@ -164,8 +164,7 @@ GIS <- reactive(input$WMU_Shp$datapath[1])
     print(paste("GIS Input=", GISInput)) #input$WMU_Shp$datapath[6])
     # GISInput <- "F:/GIS_Workspace/R_Files/A_359_Boundary_TTM.shp"
 
-    survey.area359.TTM <- readOGR(shp_original$datapath[6], shp_original$name)
-    #survey.area359.TTM <- readOGR(input$WMU_Shp$datapath[6], input$WMU_Shp$Name)#substr(basename(GISInput),1,nchar(basename(GISInput))-4))
+    survey.area359.TTM <- readOGR(input$WMU_Shp$datapath[6], input$WMU_Shp$Name)#substr(basename(GISInput),1,nchar(basename(GISInput))-4))
     # survey.areanon355 <- readOGR(dsn=StrataPolyLayerFile, layer=substr(basename(StrataPolyLayerFile),1,nchar(basename(StrataPolyLayerFile))-4))
     # survey.transects359.TTM <- readOGR(dsn=PolyLineTransflown, layer=substr(basename(PolyLineTransflown),1,nchar(basename(PolyLineTransflown))-4))
 
