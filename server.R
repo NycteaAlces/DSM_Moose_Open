d0180dbb9d820a426497bcfaf419bcec055728a1
Mike Russell <mike.russell@gov.ab.ca>
2017-07-14 08:41:22 -0600

    Update server.R

--- server.R
+++ server.R
@@ -230,4 +230,132 @@ GetShapefile <- function(InShapefile, OutShapefile){
     output$myplot3 <- renderPlot({
         plot(1:10,1:10)
         })
+    
+    
+    
+    output$MUDE_MAP <- renderPlot({
+        ####################################################################
+      #Simply create the dataframe with distance data for mapping
+           #####################################################################      
+      inFile <- DB() #input$MegaDB$datapath  #User input -- Get the Access database pathname
+     # print(inFile)
+    if (is.null(inFile))
+      return(NULL)
+    DB <- paste("Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=",inFile)
+    myconn <- odbcDriverConnect(DB)
+    strat <- sqlFetch(myconn, "strata")
+    strat_num <- nrow(strat)
+    results_num_index <- as.integer(strat_num) + 1
+
+    datasheet <- as.data.frame(sqlQuery(myconn, "select * from datasheet"))
+
+    names(datasheet) <- sub(" ", ".", names(datasheet))
+    names(datasheet) <- sub(" ", ".", names(datasheet))
+    names(datasheet) <- sub("/", "", names(datasheet))
+    names(datasheet) <- sub("/", "", names(datasheet))
+
+
+
+    transflown <- datasheet[!duplicated(datasheet[, c("Transect.ID", "Stratum")]), ]
+    transflown <- transflown[!is.na(transflown$Stratum),]
+    transflown$DistancePerp <- " "
+    transflown$MUDE.GroupSize <- " "
+    transflown$Covariate.1 <- " "
+    transflown$Covariate.2 <- " "
+    transflown <- unique(transflown)
+
+
+    datasheet.2 <- datasheet[ which(datasheet$MUDE.GroupSize >0),]
+    datasheet.2 <- unique(datasheet.2)
+
+
+    DistancePreInput.MUDE.2 <- anti_join(transflown, datasheet.2, by=c("Transect.ID","Stratum"))
+    DistancePreInput.MUDE.2 <- unique(DistancePreInput.MUDE.2)
+
+
+    DistancePreInput.MUDE <- merge(datasheet.2, DistancePreInput.MUDE.2, all=T)
+    DistancePreInput.MUDE <- unique(DistancePreInput.MUDE)
+
+
+    DistanceInput<- as.data.frame(cbind(object.ID = as.numeric(DistancePreInput.MUDE$ID), Region.Label= DistancePreInput.MUDE$Stratum,Area = as.numeric(DistancePreInput.MUDE$Stratum.Area), TID = as.numeric(DistancePreInput.MUDE$Transect.ID), TLENGTH = as.numeric(DistancePreInput.MUDE$Transect.Length), Effort=as.numeric(DistancePreInput.MUDE$Length)/1000, distance= as.numeric(DistancePreInput.MUDE$DistancePerp), size=as.numeric(DistancePreInput.MUDE$MUDE.GroupSize),CC=as.factor(DistancePreInput.MUDE$Covariate.1), Activity=as.factor(DistancePreInput.MUDE$Covariate.2)))
+
+    DistanceInput <- DistanceInput[ order(DistanceInput$Region.Label, DistanceInput$TID, DistanceInput$size), ]
+
+
+    close(myconn)
+      
+####################################################################
+      #Simply create the dataframe with distance data for mapping
+#####################################################################   
+    # WMUPolyLayerFile <- input$WMU_Shp$datapath
+    # StrataPolyLayerFile <-  input$Strata_Shp$datapath #User input -- Get the Strata shapefile
+    # PolyLineTransflown <- input$TransFlown_Shp$datapath #User input -- Get the transects shapefile
+
+    #GISInput <- GIS()
+    #print(paste("GIS Input=", GIS())) #input$WMU_Shp$datapath[6])
+    # GISInput <- "F:/GIS_Workspace/R_Files/A_359_Boundary_TTM.shp"
+
+      #Handle the file names such that Shiny doesn't get confused with shapefiles
+
+GetShapefile <- function(InShapefile, OutShapefile){
+    if (is.null(InShapefile)) 
+        return(NULL)  
+    dir<-dirname(InShapefile[1,4])
+      print(paste("Directory name:",dir))
+    for ( i in 1:nrow(InShapefile)) {
+    file.rename(InShapefile[i,4], paste0(dir,"/",InShapefile[i,1]))}
+    OutShapefile <- grep(list.files(dir, pattern="*.shp", full.names=TRUE), pattern="*.xml", inv=T, value=T)
+     }
+      
+      
+     
+      
+    survey.area359.TTM <- readOGR(GetShapefile(input$WMU_Shp), substr(basename(GetShapefile(input$WMU_Shp)),1,nchar(basename(GetShapefile(input$WMU_Shp)))-4))
+    #survey.area359.TTM <- readOGR(GetShapefile(input$WMU_Shp), substr(basename(GetShapefile(input$WMU_Shp)),1,nchar(basename(GetShapefile(input$WMU_Shp)))-4))
+    #survey.areanon355 <- readOGR(dsn=StrataPolyLayerFile, layer=substr(basename(StrataPolyLayerFile),1,nchar(basename(StrataPolyLayerFile))-4))
+    # transects <- reactive(input$TransFlown_Shp)
+    #survey.transects359.TTM <- readOGR(GetShapefile2(input$TransFlown_Shp), substr(basename(GetShapefile2(input$TransFlown_Shp)),1,nchar(basename(GetShapefile2(input$TransFlown_Shp))-4)))
+
+
+    obs.table.MUDE <- data.frame(cbind(object = DistanceInput$object.ID, Region.Label = DistanceInput$Region.Label, Sample.Label = DistanceInput$TID, distance = DistanceInput$distance, size = DistanceInput$size))
+
+    m1 <- merge(obs.table.MUDE, DistancePreInput.MUDE, by.x = "object", by.y = "ID")
+
+    grid_plot_obj <- function(fill, name, sp){
+
+
+      names(fill) <- NULL
+      row.names(fill) <- NULL
+      data <- data.frame(fill)
+      names(data) <- name
+
+      spdf <- SpatialPolygonsDataFrame(sp, data)
+      spdf@data$id <- rownames(spdf@data)
+      spdf.points <- fortify(spdf, region="id")
+      spdf.df <- join(spdf.points, spdf@data, by="id")
+
+
+      spdf.df$x <- spdf.df$long
+      spdf.df$y <- spdf.df$lat
+
+      geom_polygon(aes_string(x="x",y="y",fill=name, group="group"), data=spdf.df)
+    }
+
+
+    p <- ggplot ()
+    p <- p + geom_polygon(data = survey.area359.TTM, fill="light blue", aes(x=long, y=lat, group=group)) + coord_equal()
+    #p <- p + geom_polygon(data = survey.areanon355, fill="khaki", aes(x=long, y=lat, group=group)) + coord_equal()
+
+    #p <- p + geom_line(aes(x=long,y=lat,group=group), data = survey.transects359.TTM, colour = "gray" )
+    p <- p + geom_point(data = m1, aes(x=GrpX, y=GrpY, size = size), colour = "red", alpha=I(0.5) )
+    p <- p + labs(fill = "MDSTRATA", x = "Easting (10TM AEP Forest)", y = "Northing (10TM AEP Forest)")
+    p <- p + geom_point(aes(x=))
+
+
+      plot(p)
+    
+    
+    
+    
+    
     })
