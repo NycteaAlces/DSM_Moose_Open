df2bbac7ab832da038774e82e1ba9d8d75f17001
Mike Russell <mike.russell@gov.ab.ca>
2017-07-19 13:59:59 -0600

    Update server.R

--- server.R
+++ server.R
@@ -96,7 +96,7 @@ GIS <- reactive(input$WMU_Shp)
 
 
     model1 <- ddf(method="ds", data=DistanceInput2, dsmodel = ~cds(key="hn"), meta.data=list(width=425))
-    ddf.1.moos <- ds(DistanceInput2, key="hn", adjustment = "cos", truncation = 425)
+    ddf.1.moos <- ds(DistanceInput2, key="hn", adjustment = "cos", truncation = input$truncation)
 
 
 
