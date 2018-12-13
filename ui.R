cf3b5992a7ae96cd3e6dd5fc0bbfef652ac5bc01
Mike Russell <mike.russell@gov.ab.ca>
2018-12-13 13:26:56 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,8 +2,7 @@
 ipak <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg))
-    install.packages(new.pkg, dependencies = TRUE,repos = c("http://rstudio.org/_packages",
-                                                            "http://cran.rstudio.com"))
+    install.packages(new.pkg, dependencies = TRUE)
   # update.packages(ask = FALSE, checkBuilt = TRUE)
   sapply(pkg, require, character.only = TRUE)
 }
