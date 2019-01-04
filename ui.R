abf93d627429434c16e2835317253da40b93248a
Mike Russell <mike.russell@gov.ab.ca>
2019-01-04 14:53:08 -0700

    Update ui.R - remove chaff
    Removed the power analysis button, tab and desired CV slider as it was in testing and not currently functional

--- ui.R
+++ ui.R
@@ -20,9 +20,7 @@ shinyUI(pageWithSidebar(
               accept=c('.accdb', '.mdb')),
     fileInput('WMU_Shp', 'Step 2. Choose your WMU Polygon Shapefile File (Alberta 10TM) - Note include all shapefile components (i.e. *.shp, *.dbf, *.sbn, etc.)',
               accept=c('.shp','.dbf','.sbn','.sbx', '.shx','.prj','.cpg'), multiple=TRUE),
-    sliderInput("truncation", "Step 3: Choose right truncation distance", min=0, max=1000, value=425, step = 25),
-    sliderInput("DesiredCV", "Optional: Choose desired coefficient of variation", min=0, max=0.5, value=0.16, step=0.01),
-    actionButton("PowerAction", HTML("Power analysis <br/> - {DANGER!} - <br/>  will take several minutes"))
+    sliderInput("truncation", "Step 3: Choose right truncation distance", min=0, max=1000, value=425, step = 25))
 
   ),
   mainPanel(
@@ -31,8 +29,7 @@ shinyUI(pageWithSidebar(
       tabPanel("Mule Deer", id="Mule Deer", textOutput("MUDE_TXT"), plotOutput("MUDE_MAP"), DT::dataTableOutput('MUDE_TAB'), DT::dataTableOutput("MUDE_TAB1"), plotOutput("MD_DF"),plotOutput("MUDE_QQ"), uiOutput("MUDE_AIRDF"), DT::dataTableOutput("MUDE_TAB2")),
       tabPanel("White-tailed Deer", id="White-tailed Deer", textOutput("WTDE_TXT"),  plotOutput("WTDE_MAP"), DT::dataTableOutput("WTDE_TAB"), DT::dataTableOutput("WTDE_TAB1"),plotOutput("WTDE_DF"), plotOutput("WTDE_QQ"), uiOutput("WTDE_AIRDF"), DT::dataTableOutput("WTDE_TAB2")),
       tabPanel("Elk", id="Elk", textOutput("WAPT_TXT"), plotOutput("WAPT_MAP"),DT::dataTableOutput("WAPT_TAB"), DT::dataTableOutput("WAPT_TAB1"),plotOutput("WAPT_DF"), plotOutput("WAPT_QQ"), uiOutput("WAPT_AIRDF"), DT::dataTableOutput("WAPT_TAB2")),#,DT::dataTableOutput("WAPT_TAB"), plotOutput("WAPT_DF"), plotOutput("WAPT_QQ")),
-      tabPanel("Horses", id="Horses", textOutput("HORS_TXT"), plotOutput("HORS_MAP"),DT::dataTableOutput("HORS_TAB"), DT::dataTableOutput("HORS_TAB1"), plotOutput("HORS_DF"), plotOutput("HORS_QQ"), uiOutput("HORS_AIRDF"), DT::dataTableOutput("HORS_TAB2")),#,DT::dataTableOutput("WAPT_TAB"), plotOutput("WAPT_DF"), plotOutput("WAPT_QQ")),
-      tabPanel("Power analysis", id="Power", textOutput("POW_TXT"), plotOutput("MOOS_POW1"), plotOutput("MOOS_POW2"))
+      tabPanel("Horses", id="Horses", textOutput("HORS_TXT"), plotOutput("HORS_MAP"),DT::dataTableOutput("HORS_TAB"), DT::dataTableOutput("HORS_TAB1"), plotOutput("HORS_DF"), plotOutput("HORS_QQ"), uiOutput("HORS_AIRDF"), DT::dataTableOutput("HORS_TAB2")),#,DT::dataTableOutput("WAPT_TAB"), plotOutput("WAPT_DF"), plotOutput("WAPT_QQ")))
 
     ))
 ))
